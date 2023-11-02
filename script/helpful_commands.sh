781ca90710c6cd17245660426eb94d168e657c2f
yolandaw2021 <1621wyf@gmail.com>
2023-11-01 20:59:17 -0400

    Job submission helper files.

---
+++ script/helpful_commands.sh
@@ -0,0 +1,10 @@
+# Command for interactive session:
+srun --gres gpu:a6000:1 -n 4 -t 48:00:00 -p kilian --pty /bin/bash 
+srun --gres gpu:a6000:1 -n 4 -t 48:00:00 -p kilian --mem 32G --pty /bin/bash
+cp -r /home/ys732/share/datasets/amodal_Ithaca365 /scratch
+# Command for observing gpu condition
+watch -d -n 0.5 nvidia-smi
+# jupyter-notebook
+XDG_RUNTIME_DIR=/tmp/yw583 jupyter-notebook --ip=0.0.0.0 --port=8888
+# small runs
+srun --gres gpu:a6000:1 -n 4 -t 2:00:00 -p kilian --mem 2G --pty /bin/bash
